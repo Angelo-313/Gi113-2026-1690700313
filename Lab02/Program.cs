0e0aabc85c3b418e0c25357c3e7af668fa9f2077
Nattawut <janyanee1974@gmail.com>
2026-08-24 11:41:14 +0700

    Lab02.1

--- Lab02/Program.cs
+++ Lab02/Program.cs
@@ -1,10 +1,42 @@
-﻿namespace Lab02
+﻿/*
+ * Student ID :1690700313
+ * Name       :Nattawut Suwannit
+ * Section    :129A
+ * No.        :
+ * Course     : GI113 Computer Programming (GI)
+ */
+
+
+
+namespace Lab02
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string bossName = "Kirin";
+            char rank = 'S';
+            int level = 7;
+            int maxHp = 240;
+            int currentHp = 175;
+            float attackPower = 42.5f;
+            double critMultiplier = 1.75;
+            bool isBoss = true;
+
+            Console.WriteLine("===== BOSS STATUS: INITIAL =====");
+            Console.WriteLine($"Name: {bossName}");
+            Console.WriteLine($"Rank: {rank}");
+            Console.WriteLine($"level: {level}");
+            Console.WriteLine($"HP {currentHp} / {maxHp}");
+
+            int hpPercent = currentHp * 100 / maxHp;
+            Console.WriteLine($"HP Percent: {hpPercent}%");
+            Console.WriteLine();
+            Console.WriteLine("Kirin takes 60 damage!");
+            Console.WriteLine();
+            Console.WriteLine($"===== BOSS STATUS: AFTER DAMAGE =====");
+            Console.WriteLine($"Hp: {currentHp} / {maxHp}");
+            Console.WriteLine($"Hp Percent: 47%");
         }
     }
 }
