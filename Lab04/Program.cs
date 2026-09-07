54e107fdf970b4e3e28ab22ee2070475f4eba1ee
Angelo-313 <janyanee1974@gmail.com>
2026-09-07 11:02:50 +0700

    Update Program.cs

--- Lab04/Program.cs
+++ Lab04/Program.cs
@@ -4,28 +4,6 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("NEW HERO");
-
-            Console.WriteLine("Name your hero:"); // writeline พิมบรรทัดใหม่ wrie พิมยาวต่อเดิม
-
-            string playerName = Console.ReadLine(); //
-
-            Console.WriteLine($"\n\"Welcome, {playerName}. Your journey begins...\"");
-
-            Console.WriteLine("***************");
-            Console.WriteLine("Choose difficulty (1-3):");
-
-            int difficulty = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine($"Difficulty set to {difficulty}.");
-
-            
-
-            Console.WriteLine("*********RATE THIS LEVEL*********");
-            Console.WriteLine("Rate this level (1-5):");
-            bool isValid1 = double.TryParse(Console.ReadLine(), out double rating);
-            Console.WriteLine($"Valid input: {isValid1}");
-            Console.WriteLine($"Rating: {rating}");
-
             Console.WriteLine("*********CHARACTER CREATION*********");
             Console.WriteLine("Name your character:");
             string charName = Console.ReadLine();
